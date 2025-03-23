5ca593edae883f12205b84730f75b5d0426f7d38
DavidVSantiago <david.valente.santiago@gmail.com>
2025-03-23 11:30:41 -0300

    update

--- main_matrix.hpp
+++ main_matrix.hpp
@@ -15,15 +15,17 @@ private:
     // ATRIBUTES ------------------------------------------------------
     int alphaData[9]; // symbolic alpha data state
     int betaData[9]; // symbolic beta data state
-    int alphaSqrd[2]; // temporary stores |α|^2 and |α'|^2
-    int betaSqrd[2]; // temporary stores |β|^2 and |β'|^2
+    int alphaSqrd[2]; // temporary stores |α|^2
+    int betaSqrd[2]; // temporary stores |β|^2
+    int alphaSqrdNorm[2]; // temporary stores |α'|^2
+    int betaSqrdNorm[2]; // temporary stores |β'|^2
     int zf_wf[2]; // temporary stores "alphaSqrd" + "betaSqrd"
     bool normalized = true; // para indicar se o estado se encontra normalizado
     bool calculated = false; // para indicar se os cálculos do estado já foram realidaos
     
     // UTILS FUNCTIONS ---------------------------------------------------
     // Função para calcular o MDC usando o Algoritmo de Euclides
-    int MDC(int &a, int &b) {
+    int MDC(int a, int b) {
         while (b != 0) {
             int temp = b;
             b = a % b;
@@ -81,69 +83,52 @@ private:
     /** Calculate |α'|^2 and |β'|^2 */
     void calcAmpModSquaredNormalized(int *zf_wf){
         /** calcula x² (alpha) */
-        if(alphaData[1]==0) alphaSqrd[0] = zf_wf[1]*(alphaData[0]*alphaData[0]); // x is not root, apenas calcula wf*x² e o atribui a z' de z'/w'
-        else alphaSqrd[0] = zf_wf[1]*(alphaData[2]*alphaData[2]*alphaData[0]); // se x for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso, multiplica por wf e atribui a z' de z'/w'
+        if(alphaData[1]==0) alphaSqrdNorm[0] = zf_wf[1]*(alphaData[0]*alphaData[0]); // x is not root, apenas calcula wf*x² e o atribui a z' de z'/w'
+        else alphaSqrdNorm[0] = zf_wf[1]*(alphaData[2]*alphaData[2]*alphaData[0]); // se x for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso, multiplica por wf e atribui a z' de z'/w'
         /** calcula y² (alpha) */
-        if(alphaData[4]==0) alphaSqrd[0] += zf_wf[1]*(alphaData[3]*alphaData[3]); // y is not root, apenas calcula wf*y² e o adiciona a z' de z'/w'
-        else alphaSqrd[0] += zf_wf[1]*(alphaData[5]*alphaData[5]*alphaData[3]); // se y for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por wf e adiciona a z' de '/w'
+        if(alphaData[4]==0) alphaSqrdNorm[0] += zf_wf[1]*(alphaData[3]*alphaData[3]); // y is not root, apenas calcula wf*y² e o adiciona a z' de z'/w'
+        else alphaSqrdNorm[0] += zf_wf[1]*(alphaData[5]*alphaData[5]*alphaData[3]); // se y for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por wf e adiciona a z' de '/w'
         /** calcula d² (alpha) */
-        if(alphaData[7]==0) alphaSqrd[1] = zf_wf[0]*(alphaData[6]*alphaData[6]); // d is not root, apenas calcula zf*d² e o atribui a w' de z'/w'
-        else alphaSqrd[1] = zf_wf[0]*(alphaData[8]*alphaData[8]*alphaData[6]); // se d for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por zf e atribui a w' de z'/w'
+        if(alphaData[7]==0) alphaSqrdNorm[1] = zf_wf[0]*(alphaData[6]*alphaData[6]); // d is not root, apenas calcula zf*d² e o atribui a w' de z'/w'
+        else alphaSqrdNorm[1] = zf_wf[0]*(alphaData[8]*alphaData[8]*alphaData[6]); // se d for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por zf e atribui a w' de z'/w'
 
         /** calcula x² (beta) */
-        if(betaData[1]==0) betaSqrd[0] = zf_wf[1]*(betaData[0]*betaData[0]); // x is not root, apenas calcula wf*x² e o atribui a z' de z'/w'
-        else betaSqrd[0] = zf_wf[1]*(betaData[2]*betaData[2]*betaData[0]); // se x for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso, multiplica por wf e atribui a z' de z'/w'  
+        if(betaData[1]==0) betaSqrdNorm[0] = zf_wf[1]*(betaData[0]*betaData[0]); // x is not root, apenas calcula wf*x² e o atribui a z' de z'/w'
+        else betaSqrdNorm[0] = zf_wf[1]*(betaData[2]*betaData[2]*betaData[0]); // se x for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso, multiplica por wf e atribui a z' de z'/w'  
         /** calcula y² (beta) */
-        if(betaData[4]==0) betaSqrd[0] += zf_wf[1]*(betaData[3]*betaData[3]); // y is not root, apenas calcula wf*y² e o adiciona a z' de z'/w'
-        else betaSqrd[0] += zf_wf[1]*(betaData[5]*betaData[5]*betaData[3]); // se y for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por wf e adiciona a z' de z'/w'
+        if(betaData[4]==0) betaSqrdNorm[0] += zf_wf[1]*(betaData[3]*betaData[3]); // y is not root, apenas calcula wf*y² e o adiciona a z' de z'/w'
+        else betaSqrdNorm[0] += zf_wf[1]*(betaData[5]*betaData[5]*betaData[3]); // se y for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por wf e adiciona a z' de z'/w'
         /** calcula d² (beta) */
-        if(betaData[7]==0) betaSqrd[1] = zf_wf[0]*(betaData[6]*betaData[6]); // d is not root, apenas calcula zf*d² e o atribui a w' de z'/w'
-        else betaSqrd[1] = zf_wf[0]*(betaData[8]*betaData[8]*betaData[6]); // se d for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por zf e atribui a w' de z'/w'
+        if(betaData[7]==0) betaSqrdNorm[1] = zf_wf[0]*(betaData[6]*betaData[6]); // d is not root, apenas calcula zf*d² e o atribui a w' de z'/w'
+        else betaSqrdNorm[1] = zf_wf[0]*(betaData[8]*betaData[8]*betaData[6]); // se d for raiz, eleva o fator ao quadrado e multiplica pelo valor. Ex.: (a√b)² = a².b - Após isso,  multiplica por zf e atribui a w' de z'/w'
 
-        simplifyFraction(alphaSqrd); // simplifica (se possível) a fração z/w (alpha)
-        simplifyFraction(betaSqrd); // simplifica (se possível) a fração z/w (beta)
+        simplifyFraction(alphaSqrdNorm); // simplifica (se possível) a fração z/w (alpha)
+        simplifyFraction(betaSqrdNorm); // simplifica (se possível) a fração z/w (beta)
     }
     
-    // PRINT FUNCTIONS ---------------------------------------------------
-    string getAlphaRealAsString(){
-        string text = to_string(alphaData[0]);
-        if(alphaData[1]==0) text="√"+text;
-        if(alphaData[2]!=1) text=to_string(alphaData[2])+text;
+    // PRINT FUNCTIONS --------------------------------------------------- 
+    string getValAsString(int *data, int index){
+        if(data[index]==0) return "0";
+        string text = to_string(data[index]);
+        if(data[index+1]==2) text="√"+text;
+        if(data[index+2]!=1) text=to_string(data[index+2])+text;
         return text;
     }
-    string getAlphaImagAsString(){
-        string text = to_string(alphaData[3]);
-        if(alphaData[4]==0) text="√"+text;
-        if(alphaData[5]!=1) text=to_string(alphaData[5])+text;
-        return text;
-    }
-    string getAlphaDenAsString(){
-        string text = to_string(alphaData[3]);
-        if(alphaData[4]==0) text="√"+text;
-        if(alphaData[5]!=1) text=to_string(alphaData[5])+text;
-        return text;
-    }
+    string getAmpAsString(int *data){
+        string real = getValAsString(data,0);
+        string imag = getValAsString(data,3);
+        string den = getValAsString(data,6);
+        
+        if(imag=="1") imag=""; // remoção do valor 1
+        else if(imag=="-1") imag="-"; // remoção do valor -1
+        if(imag[0]!='-') imag="+"+imag; // adiciona o sinal de '+' apenas se não for negativo
 
-    string getAlphaRealAsString(){ return "("++"/"+getDenAsString(0)+")"; }
-    string getAlphaImagAsString(){ return "("+getImagAsString(0)+"/"+getDenAsString(0)+")"; }
-    string getBetaRealAsString(){ return "("+getRealAsString(1)+"/"+getDenAsString(1)+")"; }
-    string getBetaImagAsString(){ return "("+getImagAsString(1)+"/"+getDenAsString(1)+")"; } 
-    string getAlphaAsString(){
-        string alphaReal = getAlphaRealAsString();
-        string alphaImag = getAlphaImagAsString();
-        if(alphaReal.substr(0, 3) == "(0/") return "("+alphaImag+"i)";
-        else if(alphaImag.substr(0, 3) == "(0/") return "("+alphaReal+")";
-        else return "("+alphaReal+"+"+alphaImag+"i)";
-    }
-    string getBetaAsString(){
-        string betaReal = getBetaRealAsString();
-        string betaImag = getBetaImagAsString();
-        if(betaReal.substr(0, 3) == "(0/") return "("+betaImag+"i)";
-        else if(betaImag.substr(0, 3) == "(0/") return "("+betaReal+")";
-        else return "("+betaReal+"+"+betaImag+"i)";
+        if(real=="0") return "("+imag+"i/"+den+")"; // se não existe parte real, omite-a
+        if(imag=="0"||imag=="+0") return "("+real+"/"+den+")"; // se não existe parte imaginaria, omite-a
+        return "(("+real+imag+"i)/"+den+")"; // caso existam ambas (real e imaginária)
     }
     string getStateAsString(){
-        return "|ψ⟩ = "+getAlphaAsString()+"|0⟩ + "+getBetaAsString()+"|1⟩";
+        return "|ψ⟩ = "+getAmpAsString(alphaData)+"|0⟩ + "+getAmpAsString(betaData)+"|1⟩";
     }
     
 public:
@@ -186,11 +171,15 @@ public:
         cout << "|β|² = ("<<betaSqrd[0]<<"/"<<betaSqrd[1]<<")"<<endl;
         if(!normalized){
             cout << "Não nomalizado!" << endl;
-            cout << "|α'|² = ("<<alphaSqrd[0]<<"/"<<alphaSqrd[1]<<")"<<endl;
-            cout << "|β'|² = ("<<betaSqrd[0]<<"/"<<betaSqrd[1]<<")"<<endl;
-        }else cout << "Nomalizado!" << endl;
-        cout << "Probabilidade de medir |0⟩ = " << ((float)alphaSqrd[0]/alphaSqrd[1])*100 << "%" << endl;
-        cout << "Probabilidade de medir |1⟩ = " << ((float)betaSqrd[0]/betaSqrd[1])*100 << "%" << endl;
+            cout << "|α'|² = ("<<alphaSqrdNorm[0]<<"/"<<alphaSqrdNorm[1]<<")"<<endl;
+            cout << "|β'|² = ("<<betaSqrdNorm[0]<<"/"<<betaSqrdNorm[1]<<")"<<endl;
+            cout << "Probabilidade de medir |0⟩ = " << ((float)alphaSqrdNorm[0]/alphaSqrdNorm[1])*100 << "%" << endl;
+            cout << "Probabilidade de medir |1⟩ = " << ((float)betaSqrdNorm[0]/betaSqrdNorm[1])*100 << "%" << endl;
+        }else{
+            cout << "Nomalizado!" << endl;
+            cout << "Probabilidade de medir |0⟩ = " << ((float)alphaSqrd[0]/alphaSqrd[1])*100 << "%" << endl;
+            cout << "Probabilidade de medir |1⟩ = " << ((float)betaSqrd[0]/betaSqrd[1])*100 << "%" << endl;
+        }
         cout << "--------------------------------------------" << endl;
     }
 
